6f7186ce59e31e672793a9ef10c5bed6613baea5
luizumbra <luiz_fantasma@hotmail.com>
2015-08-07 01:04:26 -0300

    In file 'complete.R' was developed a 'complete' function with a specified directory 'directory' and id files, 'id'. The function 'complete' has a anonymous function that develope standard file names with id, read the csv files, calculate the nobs and return a specific array with id, nobs number and respective names.

--- R_Programming/complete.R
+++ R_Programming/complete.R
@@ -12,4 +12,34 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
+  
+  # Anonymous function
+  nobsResult <- lapply(id, function(x) {
+    
+    # Develop the standard file names
+    filename <- if(x < 10) {
+      paste(directory, "/00", as.character(x), ".csv", sep = "")
+    }
+    else if(x < 100) {
+      paste(directory, "/0", as.character(x), ".csv", sep = "")
+    }
+    else {
+      paste(directory, "/", as.character(x), ".csv", sep = "")
+    }
+    
+    # Read csv files
+    fileread <- read.csv(filename, header = TRUE)
+    
+    ## Calculate the nobs
+    nobsSulfate <- !is.na(fileread$sulfate)
+    nobsNitrate <- !is.na(fileread$nitrate)
+    nobsBool <- nobsSulfate & nobsNitrate
+    
+    ## Create the specified array with id, nobs and the respective names
+    nobsVec <- c(x, sum(as.integer(nobsBool)))
+    names(nobsVec) <- c("id", "nobs")
+    nobsVec
+  })
+  
+  nobsResult
 }
