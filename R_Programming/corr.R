0528bce2467be627d5556e93fa51ee51eb5521b5
luizumbra <luiz_fantasma@hotmail.com>
2015-08-07 18:56:22 -0300

    The 'complete' function of 'complete.R' file was inserted in 'corr.R' file.

--- R_Programming/corr.R
+++ R_Programming/corr.R
@@ -1,3 +1,56 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  # Anonymous function
+  nobsResult <- do.call("rbind", lapply(id, function(x) {
+    
+    # Develop the standard file names
+    filename <- if(x < 10) {
+      paste(directory, "/00", as.character(x), ".csv", sep = "")
+    }
+    else if(x < 100) {
+      paste(directory, "/0", as.character(x), ".csv", sep = "")
+    }
+    else {
+      paste(directory, "/", as.character(x), ".csv", sep = "")
+    }
+    
+    # Read csv files
+    fileread <- read.csv(filename, header = TRUE)
+    
+    ## Calculate the nobs
+    nobsSulfate <- !is.na(fileread$sulfate)
+    nobsNitrate <- !is.na(fileread$nitrate)
+    nobsBool <- nobsSulfate & nobsNitrate
+    
+    ## Create the specified array with id, nobs and the respective names
+    nobsVec <- c(as.integer(x), as.integer(sum(as.integer(nobsBool))))
+    names(nobsVec) <- c("id", "nobs")
+    nobsVec
+  }))
+  
+  #  as.array.default(nobsResult)
+  #as.vector.factor(nobsResult)
+  as.data.frame.array(nobsResult)
+  #  as.data.frame.list(nobsResult)
+  #  as.table.default(nobsResult)
+  #as.vector.factor(nobsResult)
+  #as.personList(nobsResult)
+  #nobsResult
+}
+
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
