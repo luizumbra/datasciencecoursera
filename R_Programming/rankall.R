9e5777e79a797605bb73862985eef848a809d610
luizumbra <luiz_fantasma@hotmail.com>
2015-08-09 22:47:49 -0300

    In R_Programming was add rankall.R file, in wich have the R function rankall for the exercise 4 of the programming assignment 3.

---
+++ R_Programming/rankall.R
@@ -0,0 +1,10 @@
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  
+  ## Check that state and outcome are valid
+  
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+  
+  
+}
