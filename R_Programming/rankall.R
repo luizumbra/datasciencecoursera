908bf3e97d27410302d51a8b301bc8a18cba10aa
luizumbra <luiz_fantasma@hotmail.com>
2015-08-09 23:07:30 -0300

    In R_Programming/rankall.R was copied the initializer of all other files of the Programming Assignment 3.

--- R_Programming/rankall.R
+++ R_Programming/rankall.R
@@ -6,5 +6,36 @@ rankall <- function(outcome, num = "best") {
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
   
+  # Define the outcome name and verify a invalid outcome
+  outcome <- if (tolower(outcome) == "heart attack") {
+    "Heart.Attack"
+  }
+  else if (tolower(outcome) == "heart failure") {
+    "Heart.Failure"
+  }
+  else if (tolower(outcome) == "pneumonia") {
+    "Pneumonia"
+  }
+  else {
+    stop("invalid outcome")
+  }
+  
+  # Read csv file
+  fileToOutcome <- read.csv(
+    file = "./rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv",
+    header = TRUE)
+  
+  # Separete in subdataset
+  hospitalName <- fileToOutcome[["Hospital.Name"]]
+  hospitalState <- fileToOutcome[["State"]]
+  hospitalMortality <- fileToOutcome[[
+    paste("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep = "")]]
+  
+  # Eliminate the "Not Available"
+  eliminateBool <- hospitalMortality == "Not Available"
+  hospitalName <- hospitalName[!eliminateBool]
+  hospitalState <- hospitalState[!eliminateBool]
+  hospitalMortality <- hospitalMortality[!eliminateBool]
+  
   
 }
